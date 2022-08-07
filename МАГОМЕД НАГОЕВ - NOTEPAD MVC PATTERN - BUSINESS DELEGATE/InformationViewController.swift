887308b33504f171a5caa7dc7308330916959223
Магомед Нагоев <magomed.nagoev@yandex.ru>
2022-08-07 15:29:59 +0600

    Create InformationViewController.swift

---
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/InformationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InformationViewController.swift
+//  МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE
+//
+//  Created by Нагоев Магомед on 7/8/22.
+//
+
+import UIKit
+
+class InformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
