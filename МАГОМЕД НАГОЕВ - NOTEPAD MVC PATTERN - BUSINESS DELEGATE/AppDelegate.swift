b9da1ca76d052c4676546c618b8c928c675c87b8
Магомед Нагоев <magomed.nagoev@yandex.ru>
2022-08-10 23:26:19 +0600

    Update AppDelegate.swift

--- МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/AppDelegate.swift
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/AppDelegate.swift
@@ -17,8 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         
        
-
-        window?.rootViewController = DocumentBrowserViewController(forOpening: [.text])
+        let navigationController = UINavigationController(rootViewController: DocumentBrowserViewController(forOpening: [.text]))
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         return true
     }
