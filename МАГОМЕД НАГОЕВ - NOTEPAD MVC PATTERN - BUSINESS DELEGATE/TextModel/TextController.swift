f653880201ebadb638836443184611646b4b85a6
Магомед Нагоев <magomed.nagoev@yandex.ru>
2022-08-10 22:56:37 +0600

    Create TextController.swift

---
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/TextModel/TextController.swift
@@ -0,0 +1,22 @@
+//
+//  TextController.swift
+//  МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE
+//
+//  Created by Нагоев Магомед on 9/8/22.
+//
+
+import Foundation
+
+class TextController {
+    
+    //MARK: - Properties
+    private let textViewer: TextViewer
+    
+    //MARK: - Initializer
+    init(textViewer: TextViewer) {
+        self.textViewer = textViewer
+        textViewer.updateTextView(text: "I love iOS ❤️")
+        textViewer.updateTitle(fileTitle: "Internship")
+    }
+    
+}
