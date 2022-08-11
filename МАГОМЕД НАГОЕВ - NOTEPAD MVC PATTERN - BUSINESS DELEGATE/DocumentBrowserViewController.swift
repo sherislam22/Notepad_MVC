664b2a4203091666d1a0d790def863c572af859e
Магомед Нагоев <magomed.nagoev@yandex.ru>
2022-08-11 12:08:15 +0600

    Update DocumentBrowserViewController.swift

--- МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/DocumentBrowserViewController.swift
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/DocumentBrowserViewController.swift
@@ -8,60 +8,75 @@
 //
     import UIKit
 
-class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocumentBrowserViewControllerDelegate
-{
+class DocumentBrowserViewController: UIDocumentBrowserViewController,
+                                     UIDocumentBrowserViewControllerDelegate {
+    private var template: URL?
+    private var fileManager: FileManagerModel
+    
+    init() {
+        fileManager = FileManagerModel()
+        super.init(forOpening: [.text])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         delegate = self
         allowsPickingMultipleItems = false
         allowsDocumentCreation = false
-        
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            template = try? FileManager.default.url(
-                for: .applicationSupportDirectory,
-                in: .userDomainMask,
-                appropriateFor: nil,
-                create: true
-            ).appendingPathComponent("default.ntp")
-            if template != nil {
-                allowsDocumentCreation = FileManager.default.createFile(atPath: template! .path, contents: Data())
-            }
-        }
+        createDefaultFile()
     }
-    
-    var template: URL?
     
     // MARK: UIDocumentBrowserViewControllerDelegate
     
-    func documentBrowser(_ controller: UIDocumentBrowserViewController, didRequestDocumentCreationWithHandler
-importHandler: @escaping(URL?, UIDocumentBrowserViewController.ImportMode) -> Void)
-    {
+    private func createDefaultFile() {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            template = fileManager.createNtpURL()
+
+            if let templateURL = template {
+                allowsDocumentCreation = fileManager.createNtpFile(url: templateURL)
+            }
+        }
+    }
+
+    func documentBrowser(_ controller:
+                         UIDocumentBrowserViewController,
+                         didRequestDocumentCreationWithHandler importHandler:
+                         @escaping(URL?, UIDocumentBrowserViewController.ImportMode) -> Void) {
         importHandler(template, .copy)
     }
+
     func documentBrowser(_ controller:UIDocumentBrowserViewController,
                          didPickDocumentURLs documentURLs: [URL]) {
         guard let sourceURL = documentURLs.first else { return }
         presentDocument(at: sourceURL)
     }
+
     func documentBrowser(_ controller: UIDocumentBrowserViewController,
                          didImportDocumentAt sourceURL: URL,
                          toDestinationURL destinationURL: URL){
         presentDocument(at: destinationURL)
     }
+
     func documentBrowser(_ controller: UIDocumentBrowserViewController, failedToImportDocumentAt documentURL: URL, error: Error?){
         print("error" + (String(describing: error)))
     }
-    
+
     // MARK: Document Presentation
-    
+
     func presentDocument(at documentURL: URL){
         let textViewController = TextViewer()
+
         let navigationController = UINavigationController(rootViewController: textViewController)
         navigationController.modalPresentationStyle = .fullScreen
+
         present(navigationController, animated: true)
-//        let text = FileManagerModel.openFile(fileNamePath: documentURL.path)
-        textViewController.document = TextDocument(fileURL: documentURL)
-//        textViewController.updateTextView(text: text)
+
+        textViewController.getDocumentURL(url: documentURL)
+
     }
     
 }
