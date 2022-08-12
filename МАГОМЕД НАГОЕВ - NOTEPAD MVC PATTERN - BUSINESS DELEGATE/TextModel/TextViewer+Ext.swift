0423fe7870f5d44815d3a15674590e9d813350b9
Памирбек Алмазбеков <pauchok006@gmail.com>
2022-08-12 11:43:52 +0200

    Create TextViewer+Ext.swift
    Adding Font Picker Functionality

---
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/TextModel/TextViewer+Ext.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+extension TextViewer: UIFontPickerViewControllerDelegate {
+    
+    @objc func didTapPickFont(){
+        let config = UIFontPickerViewController.Configuration()
+        config.includeFaces = true
+        let fontPicker = UIFontPickerViewController(configuration: config)
+        fontPicker.delegate = self
+        present(fontPicker, animated: true)
+        
+    }
+    
+    func fontPickerViewControllerDidCancel(_ viewController: UIFontPickerViewController) {
+        viewController.dismiss(animated: true, completion: nil)
+    }
+
+    func fontPickerViewControllerDidPickFont(_ viewController: UIFontPickerViewController) {
+        viewController.dismiss(animated: true, completion: nil)
+        guard let descriptor = viewController.selectedFontDescriptor else {return}
+        let selectedFont = UIFont(descriptor: descriptor, size: 24)
+        print(selectedFont)
+    }
+}
