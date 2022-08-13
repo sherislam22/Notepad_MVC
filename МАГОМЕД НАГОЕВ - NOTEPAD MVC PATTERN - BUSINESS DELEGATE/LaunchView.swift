2d2a6b9572d73fb93399a76e3bf77ea6147e0b6a
Магомед Нагоев <magomed.nagoev@yandex.ru>
2022-08-13 23:52:16 +0600

    Update LaunchView.swift

--- МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/LaunchView.swift
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/LaunchView.swift
@@ -12,9 +12,7 @@ class LaunchView: UIView {
     private let foldersView: UIImageView
     private let logoView: UIImageView
     private let pointsView: UIImageView
-    private let stackView: UIStackView
-    
-    private let logoName: UILabel
+
 
     override init(frame: CGRect) {
         foldersView = UIImageView()
@@ -22,10 +20,7 @@ class LaunchView: UIView {
 
         logoView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
         pointsView = UIImageView()
-        stackView = UIStackView()
         
-        logoName = UILabel()
-
         super.init(frame: frame)
         backgroundColor = .red
         uiConfig()
@@ -37,18 +32,10 @@ class LaunchView: UIView {
 
     private func uiConfig(){
         backgroundColor = #colorLiteral(red: 0.1176470444, green: 0.1176470444, blue: 0.1176470444, alpha: 1)
-        
-        logoName.text = "NOTEPAD"
-        logoName.textColor = .white
-        logoName.textAlignment = .center
-        logoName.font = .systemFont(ofSize: 18, weight: .medium)
 
         addSubview(foldersView)
         addSubview(logoView)
-        addSubview(logoName)
         addSubview(pointsView)
-        addSubview(stackView)
-        
 
         foldersView.image = UIImage(named: "folders")
         logoView.image = UIImage(named: "logo")
@@ -65,11 +52,11 @@ class LaunchView: UIView {
            let widthPoints = pointsView.image?.size.width,
            let heightPoints = pointsView.image?.size.height {
 
-            logoView.heightAnchor.constraint(equalToConstant: heightLogo * 0.6).isActive = true
-            logoView.widthAnchor.constraint(equalToConstant: widthLogo * 0.6).isActive = true
+            logoView.heightAnchor.constraint(equalToConstant: heightLogo * 0.11).isActive = true
+            logoView.widthAnchor.constraint(equalToConstant: widthLogo * 0.11).isActive = true
             
-            pointsView.heightAnchor.constraint(equalToConstant: heightPoints * 0.1).isActive = true
-            pointsView.widthAnchor.constraint(equalToConstant: widthPoints * 0.1).isActive = true
+            pointsView.heightAnchor.constraint(equalToConstant: heightPoints * 0.06).isActive = true
+            pointsView.widthAnchor.constraint(equalToConstant: widthPoints * 0.06).isActive = true
         }
     }
     
@@ -78,31 +65,18 @@ class LaunchView: UIView {
         
         foldersView.translatesAutoresizingMaskIntoConstraints = false
         logoView.translatesAutoresizingMaskIntoConstraints = false
-        logoName.translatesAutoresizingMaskIntoConstraints = false
         pointsView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-
-        stackView.addArrangedSubview(logoView)
-        stackView.addArrangedSubview(logoName)
-        stackView.spacing = 5
-        stackView.alignment = .center
-        stackView.distribution = .fill
         
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 50),
-            stackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            logoView.topAnchor.constraint(equalTo: topAnchor, constant: 50),
+            logoView.centerXAnchor.constraint(equalTo: centerXAnchor),
 
-            logoName.centerYAnchor.constraint(equalTo: logoView.centerYAnchor),
-            logoName.leftAnchor.constraint(equalTo: logoView.rightAnchor, constant: 5),
-
-            
             foldersView.centerYAnchor.constraint(equalTo: centerYAnchor),
             foldersView.centerXAnchor.constraint(equalTo: centerXAnchor),
             foldersView.heightAnchor.constraint(equalToConstant: 234),
             foldersView.widthAnchor.constraint(equalToConstant: 128.7),
             
-            pointsView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            pointsView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -35),
             pointsView.centerXAnchor.constraint(equalTo: centerXAnchor)
             
         ])
@@ -119,7 +93,6 @@ class LaunchView: UIView {
     private func disappearLaunchView() {
         UIView.animate(withDuration: 1.0) { [weak self] in
             self?.foldersView.alpha = 0
-            self?.logoName.alpha = 0
             self?.logoView.alpha = 0
             self?.pointsView.alpha = 0
         } completion: { [weak self] _ in
