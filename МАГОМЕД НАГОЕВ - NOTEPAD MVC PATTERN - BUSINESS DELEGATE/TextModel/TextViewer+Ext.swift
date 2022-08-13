862934a24c18cfb0bd66e68181f522ce26ba66a2
Памирбек Алмазбеков <pauchok006@gmail.com>
2022-08-13 18:27:07 +0200

    Add Alert with size PickerView
    Added Alert with PickerView for changing size of font in TextViewer

--- МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/TextModel/TextViewer+Ext.swift
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/TextModel/TextViewer+Ext.swift
@@ -1,6 +1,40 @@
 import UIKit
 
-extension TextViewer: UIFontPickerViewControllerDelegate {
+//    надо добавить функции в TextViewer и объектную переменную FontSizePickerAlert
+//
+//    public func setTextViewFont(_ font: UIFont){
+//        textView.font = font
+//    }
+//
+//    public func getTextViewFont() -> UIFont {
+//        guard let font = textView.font else { return UIFont()}
+//        return font
+//    }
+//
+//    public func getFontSizePickerAlert() -> FontSizePickerAlert {
+//        return fontSizePickerAlert!
+//    }
+
+extension TextViewer: UIFontPickerViewControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        let fontSizePickerAlert = getFontSizePickerAlert()
+        return fontSizePickerAlert.getFontSizes().count
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let fontSizePickerAlert = getFontSizePickerAlert()
+        return String(fontSizePickerAlert.getFontSizes()[row])
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let fontSizePickerAlert = getFontSizePickerAlert()
+        fontSizePickerAlert.setFontValue(UIFont(name: self.getTextViewFont().fontName, size: CGFloat(fontSizePickerAlert.getFontSizes()[row]))!)
+    }
     
     @objc func didTapPickFont(){
         let config = UIFontPickerViewController.Configuration()
@@ -9,6 +43,26 @@ extension TextViewer: UIFontPickerViewControllerDelegate {
         fontPicker.delegate = self
         present(fontPicker, animated: true)
         
+    }
+    
+    @objc func testButtonSize(){
+        let alert = UIAlertController(title: "Select size", message: "\n\n\n\n\n\n", preferredStyle: .alert)
+        let fontSizePickerView = UIPickerView(frame: CGRect(x: 5, y: 30, width: 250, height: 140))
+        let fontSizePickerAlert = getFontSizePickerAlert()
+        fontSizePickerAlert.setFontValue(UIFont(name: self.getTextViewFont().fontName, size: CGFloat(fontSizePickerAlert.getFontSizes()[0]))!)
+        
+        alert.view.addSubview(fontSizePickerView)
+        fontSizePickerView.dataSource = self
+        fontSizePickerView.delegate = self
+
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (UIAlertAction) in
+            
+            
+            self.setTextViewFont(fontSizePickerAlert.getFontValue())
+            
+        }))
+        self.present(alert, animated: true, completion: nil )
     }
     
     func fontPickerViewControllerDidCancel(_ viewController: UIFontPickerViewController) {
@@ -20,6 +74,6 @@ extension TextViewer: UIFontPickerViewControllerDelegate {
         guard let descriptor = viewController.selectedFontDescriptor else {return}
         let selectedFont = UIFont(descriptor: descriptor, size: 24)
 //        функция для установления шрифта с типом аргумента UIFont
-//        setTextViewFont(selectedFont)
+        setTextViewFont(selectedFont)
     }
 }
