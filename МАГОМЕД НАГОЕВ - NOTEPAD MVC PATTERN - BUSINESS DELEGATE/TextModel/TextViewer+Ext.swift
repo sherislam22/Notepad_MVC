05ef848f4cadb23e670520d176cd9de54f443509
Памирбек Алмазбеков <pauchok006@gmail.com>
2022-08-13 21:55:55 +0200

    Add deleting and saving file name alerts
    Added objc functions for viewing the alerts when deleting and saving file name

--- МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/TextModel/TextViewer+Ext.swift
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/TextModel/TextViewer+Ext.swift
@@ -1,6 +1,10 @@
 import UIKit
 
-//    надо добавить функции в TextViewer и объектную переменную FontSizePickerAlert
+//    в TextViewer надо добавить функции и объектную переменную FontSizePickerAlert
+//
+//    private var fontSizePickerAlert: FontSizePickerAlert?
+//
+//    fontSizePickerAlert = FontSizePickerAlert(self)
 //
 //    public func setTextViewFont(_ font: UIFont){
 //        textView.font = font
@@ -45,7 +49,7 @@ extension TextViewer: UIFontPickerViewControllerDelegate, UIPickerViewDelegate, 
         
     }
     
-    @objc func testButtonSize(){
+    @objc func didTapPickFontSize(){
         let alert = UIAlertController(title: "Select size", message: "\n\n\n\n\n\n", preferredStyle: .alert)
         let fontSizePickerView = UIPickerView(frame: CGRect(x: 5, y: 30, width: 250, height: 140))
         let fontSizePickerAlert = getFontSizePickerAlert()
@@ -64,6 +68,47 @@ extension TextViewer: UIFontPickerViewControllerDelegate, UIPickerViewDelegate, 
         }))
         self.present(alert, animated: true, completion: nil )
     }
+    
+    @objc func didTapSaveButton(){
+        let alert = UIAlertController(title: "Name the file", message: nil, preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "Save", style: .default) { _ in
+            if let txtField = alert.textFields?.first, let text = txtField.text {
+                // operations
+                print("Test ////// File name: " + text)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in }
+        
+        alert.view.addSubview(UIView())
+        alert.addTextField { (textField) in
+            textField.placeholder = "File name"
+        }
+        alert.addAction(confirmAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @objc func didTapNewFile(){
+        let alert = UIAlertController(title: "Delete current file?", message: "Are you sure you want to delete the current file and open new?", preferredStyle: .alert)
+
+        let confirmAction = UIAlertAction(title: "Delete", style: .destructive,
+                                      handler: {(_: UIAlertAction!) in
+                                        print("Test ////// deleting file")
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: { _ in })
+        
+        alert.addAction(cancelAction)
+        alert.addAction(confirmAction)
+        alert.view.addSubview(UIView())
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+
     
     func fontPickerViewControllerDidCancel(_ viewController: UIFontPickerViewController) {
         viewController.dismiss(animated: true, completion: nil)
