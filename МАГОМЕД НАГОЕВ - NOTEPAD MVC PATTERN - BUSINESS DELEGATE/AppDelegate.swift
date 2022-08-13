0d7b88257bf93f0c1d5b6d6ee01b3ef280fe0e7d
Магомед Нагоев <magomed.nagoev@yandex.ru>
2022-08-13 18:37:47 +0600

    Change rootViewController

--- МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/AppDelegate.swift
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/AppDelegate.swift
@@ -16,8 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         
-       
-        window?.rootViewController = DocumentBrowserViewController()
+        let textViewer = TextViewer()
+        let navigationController = UINavigationController(rootViewController: textViewer)
+        
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         return true
     }
