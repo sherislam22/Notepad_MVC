5cb32f4ab716a79a064b4dab5d2ef0c32352f801
Магомед Нагоев <magomed.nagoev@yandex.ru>
2022-08-14 23:09:01 +0600

    Create Router.swift

---
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/Router.swift
@@ -0,0 +1,38 @@
+//
+//  Router.swift
+//  МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE
+//
+//  Created by Нагоев Магомед on 14/8/22.
+//
+
+import Foundation
+import UIKit
+
+protocol RouterProtocol {
+    func initialViewController()
+    func pushContentMenu()
+}
+
+class Router: RouterProtocol {
+
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func initialViewController() {
+        let textViewer = TextViewer(router: self)
+
+        navigationController.viewControllers = [textViewer]
+    }
+    
+    func pushContentMenu() {
+        let menuViewer = MenuViewController(router: self)
+        navigationController.pushViewController(menuViewer, animated: true)
+    }
+    
+    func closeContentMenu() {
+        navigationController.popToRootViewController(animated: true)
+    }
+}
