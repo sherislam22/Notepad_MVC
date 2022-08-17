6e08d436d91d9722373cea8e0f550a003bafcd81
Магомед Нагоев <magomed.nagoev@yandex.ru>
2022-08-18 01:14:52 +0600

    Create MenuViewer.swift

---
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/ContentMenu/MenuViewer.swift
@@ -0,0 +1,77 @@
+//
+//  MenuViewController.swift
+//  МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE
+//
+//  Created by begaiym akunova on 13/8/22.
+//
+
+import UIKit
+
+class MenuViewer: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private let tableView: UITableView
+    public var menuController: MenuController?
+    
+    init() {
+        tableView = UITableView()
+    
+        super.init(nibName: nil, bundle: nil)
+
+        tableView.backgroundColor = nil
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.backgroundColor = .gray
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.bounds.size.width, height: view.bounds.size.height)
+
+    }
+
+    //table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MenuOptions.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = MenuOptions.allCases[indexPath.row].rawValue
+        cell.textLabel?.textColor = .white
+        cell.imageView?.image = UIImage(systemName: MenuOptions.allCases[indexPath.row].imageName)
+        cell.imageView?.tintColor = .white
+        cell.backgroundColor = .gray
+        cell.contentView.backgroundColor = nil
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = MenuOptions.allCases[indexPath.row]
+        switch item {
+        case .new:
+            break
+        case .open:
+            menuController?.router.pushDocumentViewer()
+        case .save:
+            menuController?.save()
+        case .saveAs:
+            print("change file type manager save")
+        case .print:
+            break
+        case .info:
+            navigationController?.pushViewController(InformationViewController(), animated: true)
+        }
+    }
+}
