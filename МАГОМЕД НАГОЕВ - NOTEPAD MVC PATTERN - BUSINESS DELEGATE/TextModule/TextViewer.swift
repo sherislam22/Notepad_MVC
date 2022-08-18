55e7ca990124d3cca07d3136e5b6c1cc9958a648
Наргиза Бейшекенова <beishekenovanargiza@gmail.com>
2022-08-18 18:23:20 +0600

    refactoring textView, add stackView for search and replace views

--- МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/TextModule/TextViewer.swift
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/TextModule/TextViewer.swift
@@ -10,21 +10,30 @@ import UIKit
 class TextViewer: UIViewController {
     
     //MARK: - Properties
-    private let textView: UITextView
     public var textController: TextController?
-    private var textViewBottomConstraint: NSLayoutConstraint?
-    let notePadToolBar: NotePadToolBar
+    
     private let notepadView: UIImageView
+    private let stackView: UIStackView
+    private let textView: UITextView
+    let notePadToolBar: NotePadToolBar
+    private let searchAndReplaceView: SearchAndReplaceView
+    private let searchAndReplaceButtonView: SearchAndReplaceButtonsView
+    
+    private var stackViewBottomConstraint: NSLayoutConstraint?
+    
     private var urlFile: URL
     public var filename: String?
+    
     //MARK: - Initialize
     
     init() {
+        notepadView = UIImageView(image: UIImage(named: "notepad"))
+        stackView = UIStackView()
         textView = UITextView()
         notePadToolBar = NotePadToolBar(frame: CGRect(x: 0, y: 0, width: 375, height: 44))
-        self.urlFile = URL(fileURLWithPath: "")
-        notepadView = UIImageView()
-        notepadView.image = UIImage(named: "notepad")
+        searchAndReplaceView = SearchAndReplaceView()
+        searchAndReplaceButtonView = SearchAndReplaceButtonsView()
+        urlFile = URL(fileURLWithPath: "")
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -35,51 +44,23 @@ class TextViewer: UIViewController {
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        textView.delegate = self
-        setImageView()
-        setTextView()
+        
         view.backgroundColor = .white
+        
+        setupImageView()
+        setupStackView()
+        setupTextView()
+        setupSearchAndReplaceView()
+        setupSearchAndReplaceButtonView()
         setupDismissKeyboardGesture()
-        setupKeyboardHiding()
-        notePadToolBar.translatesAutoresizingMaskIntoConstraints = false
-        textView.inputAccessoryView = notePadToolBar
-        setnavigationBar()
-        setZoom()
+        setupKeyboardFrame()
         setupNavigationItem()
-
-    }
-    
-    //MARK: - Methods
-    
-    func setZoom() {
-        textView.minimumZoomScale = 0.5
-        textView.maximumZoomScale = 2.0
-    }
-    
-    func setnavigationBar() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.dash"), style: .done, target: self, action: #selector(menuButtonTapped))
-    }
-    
-    private func setTextView() {
-        let safeArea = view.safeAreaLayoutGuide
         
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.backgroundColor = .white
-        textView.textColor = .black
-        textViewBottomConstraint = textView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        setupZoom()
         
-        guard let textViewBottomConstraint = textViewBottomConstraint else { return }
-        
-        view.addSubview(textView)
-        NSLayoutConstraint.activate([
-            textView.topAnchor.constraint(equalTo: notepadView.bottomAnchor, constant: 0),
-            textViewBottomConstraint,
-            textView.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 5),
-            textView.rightAnchor.constraint(equalTo: safeArea.rightAnchor, constant: -5)
-        ])
     }
-    
-    func setImageView() {
+    //MARK: - SetupMethods
+    private func setupImageView() {
         let safeArea = view.safeAreaLayoutGuide
         
         notepadView.translatesAutoresizingMaskIntoConstraints = false
@@ -93,6 +74,90 @@ class TextViewer: UIViewController {
         ])
     }
     
+    private func setupStackView() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackViewBottomConstraint = view.bottomAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 0)
+        stackViewBottomConstraint?.priority = .defaultHigh
+        let requiredBottomConstraint = safeArea.bottomAnchor.constraint(greaterThanOrEqualTo: stackView.bottomAnchor)
+        
+        guard let stackViewBottomConstraint = stackViewBottomConstraint else { return }
+        
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: notepadView.bottomAnchor, constant: 0),
+            stackViewBottomConstraint,
+            stackView.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 5),
+            stackView.rightAnchor.constraint(equalTo: safeArea.rightAnchor, constant: -5),
+            requiredBottomConstraint
+        ])
+        
+        stackView.axis = .vertical
+    }
+    
+    private func setupTextView() {
+        textView.delegate = self
+        
+        textView.backgroundColor = .white
+        textView.textColor = .black
+        
+        stackView.addArrangedSubview(textView)
+        
+        notePadToolBar.translatesAutoresizingMaskIntoConstraints = false
+        textView.inputAccessoryView = notePadToolBar
+    }
+    
+    private func setupSearchAndReplaceView() {
+        stackView.insertArrangedSubview(searchAndReplaceView, at: 0)
+    }
+    
+    private func setupSearchAndReplaceButtonView() {
+        stackView.addArrangedSubview(searchAndReplaceButtonView)
+    }
+    
+    private func setupDismissKeyboardGesture() {
+        let dismissKeyboardTap = UITapGestureRecognizer(target: self, action: #selector(viewTapped(_: )))
+        view.addGestureRecognizer(dismissKeyboardTap)
+    }
+    
+    @objc private func viewTapped(_ recognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    private func setupKeyboardFrame() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillChangeFrame),
+                                               name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    func setupNavigationItem() {
+        let undo = UIBarButtonItem(image: UIImage(systemName: "arrow.uturn.backward.circle"),
+                                   style: .plain,
+                                   target: self,
+                                   action: #selector(undoDidTap))
+        
+        let redo = UIBarButtonItem(image: UIImage(systemName: "arrow.uturn.forward.circle"),
+                                   style: .plain,
+                                   target: self,
+                                   action: #selector(redoDidTap))
+        
+        navigationItem.leftBarButtonItems = [undo, redo]
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.dash"),
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(menuButtonTapped))
+    }
+    
+    func setupZoom() {
+        textView.minimumZoomScale = 0.5
+        textView.maximumZoomScale = 2.0
+    }
+    
+    
+    //MARK: - Methods
+    
+    
     public func updateTextView(text: String) {
         textView.text = text
     }
@@ -104,34 +169,30 @@ class TextViewer: UIViewController {
     public func updateTitle(fileTitle: String) {
         title = fileTitle
     }
-
     
     @objc func menuButtonTapped() {
         textController?.showMenu()
     }
     
     func didTapSaveButton(){
-            let alert = UIAlertController(title: "Name the file", message: nil, preferredStyle: .alert)
-    
-            let confirmAction = UIAlertAction(title: "Save", style: .default) { _ in
-                if let txtField = alert.textFields?.first, let text = txtField.text {
-                    // operations
-                    self.filename = text
-                    self.textController?.saveAs()
-                    
-                }
-            }
-            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in }
-            alert.view.addSubview(UIView())
-            alert.addTextField { (textField) in
-                textField.placeholder = "File name"
-            }
-            alert.addAction(confirmAction)
-            alert.addAction(cancelAction)
-    
-            present(alert, animated: true, completion: nil)
-     
+        let alert = UIAlertController(title: "Name the file", message: nil, preferredStyle: .alert)
         
+        let confirmAction = UIAlertAction(title: "Save", style: .default) { _ in
+            if let txtField = alert.textFields?.first, let text = txtField.text {
+                // operations
+                self.filename = text
+                self.textController?.saveAs()
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in }
+        alert.view.addSubview(UIView())
+        alert.addTextField { (textField) in
+            textField.placeholder = "File name"
+        }
+        alert.addAction(confirmAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
     }
     
     func getFilename() -> String {
@@ -142,55 +203,13 @@ class TextViewer: UIViewController {
 extension TextViewer {
     
     // MARK: - Keyboard
-    private func setupDismissKeyboardGesture() {
-        let dismissKeyboardTap = UITapGestureRecognizer(target: self, action: #selector(viewTapped(_: )))
-        view.addGestureRecognizer(dismissKeyboardTap)
-    }
     
-    private func setupKeyboardHiding() {
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillShow),
-                                               name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillHide),
-                                               name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @objc func viewTapped(_ recognizer: UITapGestureRecognizer) {
-        view.endEditing(true)
-    }
-    
-    @objc func keyboardWillShow(sender: NSNotification) {
+    @objc private func keyboardWillChangeFrame(sender: Notification) {
         guard let userInfo = sender.userInfo,
               let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
-        if textViewBottomConstraint?.constant == 0 {
-            
-            let keyboardHeight = keyboardFrame.cgRectValue.height
-            textViewBottomConstraint?.constant = -keyboardHeight
-            
-            view.layoutIfNeeded()
-        }
-    }
-    
-    @objc func keyboardWillHide(notification: NSNotification) {
-        textViewBottomConstraint?.constant = 0
+        let intersection = view.frame.intersection(keyboardFrame.cgRectValue)
+        stackViewBottomConstraint?.constant = intersection.height
         view.layoutIfNeeded()
-    }
-    
-    func setupNavigationItem() {
-        let undo = UIBarButtonItem(image: UIImage(systemName: "arrow.uturn.backward.circle"),
-                                   style: .plain,
-                                   target: self,
-                                   action: #selector(undoDidTap))
-    
-        let redo = UIBarButtonItem(image: UIImage(systemName: "arrow.uturn.forward.circle"),
-                                   style: .plain,
-                                   target: self,
-                                   action: #selector(redoDidTap))
-        
-        navigationItem.leftBarButtonItems
-        = [undo,
-           redo]
     }
     
     @objc
@@ -201,10 +220,8 @@ extension TextViewer {
     @objc
     func redoDidTap() {
         textController?.careTaker.redo()
-
     }
 }
-
 //MARK: - Add undo and redo command
 extension TextViewer: TextWriterProtocol {
     func saveState() -> TextMemento {
