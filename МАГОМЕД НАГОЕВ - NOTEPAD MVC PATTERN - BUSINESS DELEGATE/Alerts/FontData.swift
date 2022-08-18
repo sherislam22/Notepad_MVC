d535407111673fa9db1a12c45555cd54a9e71b3b
Памирбек Алмазбеков <pauchok006@gmail.com>
2022-08-18 11:52:15 +0200

    devFontPicker version 2.0

--- МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/Alerts/FontData.swift
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/Alerts/FontData.swift
@@ -5,6 +5,7 @@ class FontData {
     private var fontValue: UIFont
     private var viewer: TextViewer
     
+    
     init(_ viewer: TextViewer) {
         fontSizes = Array(16...47)
         self.viewer = viewer
@@ -22,5 +23,81 @@ class FontData {
     func setFontValue(_ font: UIFont) {
         self.fontValue = font
     }
+    
+    //    ALERT: Deleting current file
+        
+    @objc func didTapNewFile() {
+        let alert = UIAlertController(title: "Delete current file?", message: "Are you sure you want to delete the current file and open new?", preferredStyle: .alert)
+
+        let confirmAction = UIAlertAction(title: "Delete", style: .destructive,
+                                      handler: {(_: UIAlertAction!) in
+                                        print("Test ////// deleting file")
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: { _ in })
+        
+        alert.addAction(cancelAction)
+        alert.addAction(confirmAction)
+        alert.view.addSubview(UIView())
+        
+        viewer.present(alert, animated: true, completion: nil)
+    }
+
+//    ALERT: Saving file name
+
+    @objc func didTapSaveButton() {
+        let alert = UIAlertController(title: "Name the file", message: nil, preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "Save", style: .default) { _ in
+            if let txtField = alert.textFields?.first, let text = txtField.text {
+                // operations
+                print("Test ////// File name: " + text)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in }
+        
+        alert.view.addSubview(UIView())
+        alert.addTextField { (textField) in
+            textField.placeholder = "File name"
+        }
+        alert.addAction(confirmAction)
+        alert.addAction(cancelAction)
+        
+        self.viewer.present(alert, animated: true, completion: nil)
+    }
+    
+//    PICKER VIEW: Picking font family and style
+    
+    @objc func didTapPickFont(){
+        let config = UIFontPickerViewController.Configuration()
+        config.includeFaces = true
+        let fontPicker = UIFontPickerViewController(configuration: config)
+        fontPicker.delegate = self.viewer
+        self.viewer.present(fontPicker, animated: true)
+        
+    }
+    
+    //    ALERT WITH PICKER VIEW: Picking the size of the font
+    
+    @objc func didTapPickFontSize(){
+        let alert = UIAlertController(title: "Select size", message: "\n\n\n\n\n\n", preferredStyle: .alert)
+        let fontSizePickerView = UIPickerView(frame: CGRect(x: 5, y: 30, width: 250, height: 140))
+        
+        let fontFamilyName = self.viewer.getTextViewFont().fontName
+        fontValue = UIFont(name: fontFamilyName, size: CGFloat(fontSizes[0]))!
+        
+        alert.view.addSubview(fontSizePickerView)
+        
+        fontSizePickerView.dataSource = self.viewer
+        fontSizePickerView.delegate = self.viewer
+
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (UIAlertAction) in
+            
+            self.viewer.setTextViewFont(self.fontValue)
+            
+        }))
+        self.viewer.present(alert, animated: true, completion: nil )
+    }
 }
- 
