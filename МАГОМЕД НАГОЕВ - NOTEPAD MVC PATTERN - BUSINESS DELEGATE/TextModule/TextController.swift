5b0624f9c9688c190288e7d69676fbde47f5c39b
Шерислам Талантбеков <talantbekovsherislam@gmail.com>
2022-08-18 12:20:14 +0600

    Update TextController.swift

--- МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/TextModule/TextController.swift
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/TextModule/TextController.swift
@@ -72,7 +72,7 @@ class TextController {
         careTaker.states.removeAll()
         if urlPath != "" {
             let file = fileManager.openFile(fileNamePath: urlPath)
-            textViewer.updateTextView(text: file)
+            textViewer.updateTextView(text: textViewer.getFilename())
         }
     }
 }
