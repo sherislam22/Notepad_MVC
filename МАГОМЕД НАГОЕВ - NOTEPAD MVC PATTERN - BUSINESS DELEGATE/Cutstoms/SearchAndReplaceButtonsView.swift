9c997c8b247d5d471d4f32537438ac2ec9ab01fa
Наргиза Бейшекенова <beishekenovanargiza@gmail.com>
2022-08-18 16:55:16 +0600

    Moved Views to separate files

---
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/Cutstoms/SearchAndReplaceButtonsView.swift
@@ -0,0 +1,51 @@
+//
+//  SearchAndReplaceButtonsView.swift
+//  МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE
+//
+//  Created by Nargiza Beishekenova on 18/8/22.
+//
+
+import UIKit
+
+class SearchAndReplaceButtonsView: UIView {
+    private let horizontalStackView: UIStackView
+    private let backButton: UIButton
+    private let nextButton: UIButton
+    private let replaceButton: UIButton
+    private let replaceAllButton: UIButton
+    
+    override init(frame: CGRect) {
+        horizontalStackView = UIStackView()
+        backButton = UIButton(type: .system)
+        nextButton = UIButton(type: .system)
+        replaceButton = UIButton(type: .system)
+        replaceAllButton = UIButton(type: .system)
+        super.init(frame: frame)
+        horizontalStackView.axis = .horizontal
+        horizontalStackView.spacing = 12
+        horizontalStackView.alignment = .bottom
+        horizontalStackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(horizontalStackView)
+        NSLayoutConstraint.activate([
+            horizontalStackView.topAnchor.constraint(equalTo: topAnchor, constant: 12),
+            horizontalStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12),
+            horizontalStackView.leftAnchor.constraint(equalTo: leftAnchor, constant: 12),
+            horizontalStackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -12)
+        ])
+        horizontalStackView.distribution = .equalSpacing
+        horizontalStackView.addArrangedSubview(backButton)
+        horizontalStackView.addArrangedSubview(replaceButton)
+        horizontalStackView.addArrangedSubview(replaceAllButton)
+        horizontalStackView.addArrangedSubview(nextButton)
+        
+        backButton.setImage(UIImage(systemName: "chevron.backward.circle"), for: .normal)
+        nextButton.setImage(UIImage(systemName: "chevron.forward.circle"), for: .normal)
+        replaceButton.setTitle("Заменить", for: .normal)
+        replaceAllButton.setTitle("Заменить все", for: .normal)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
