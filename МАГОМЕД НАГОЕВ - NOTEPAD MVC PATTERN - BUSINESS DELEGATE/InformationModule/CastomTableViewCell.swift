10b4c8e57107f071f787a6ca5a4108383923d3db
Бекболсун Таалайбеков <bekbolsunjr@gmail.com>
2022-08-19 22:59:55 +0600

    Исправил размеры которые не сходились

--- МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/InformationModule/CastomTableViewCell.swift
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/InformationModule/CastomTableViewCell.swift
@@ -13,21 +13,21 @@ class CastomTableViewCell: UITableViewCell {
     
     lazy var backeView: UIView = {
         //et view = UIView(frame: CGRect(x: 10, y: 6, width: self.frame.width - 20, height: 110))
-        let view = UIView(frame: CGRect(x: 10, y: 10, width: self.frame.width - 5, height: 100))
+        let view = UIView(frame: CGRect(x: 7, y: 7, width: self.frame.width - 1, height: 75))
         view.backgroundColor = UIColor.white
         return view
     }()
     
     lazy var userImage: UIImageView = {
-        let image = UIImageView(frame: CGRect(x: 4, y: 4, width: 90, height: 90))
+        let image = UIImageView(frame: CGRect(x: 4, y: 4, width: 70, height: 70))
         image.contentMode = .scaleAspectFill
         return image
     }()
     
     lazy var userLabel: UILabel = {
-        let lbl = UILabel(frame: CGRect(x: 116, y: 42, width: backeView.frame.width - 116, height: 30))
+        let lbl = UILabel(frame: CGRect(x: 90, y: 28, width: 150, height: 30))
         lbl.textAlignment = .left
-        lbl.font = UIFont.boldSystemFont(ofSize: 18)
+        lbl.font = UIFont.boldSystemFont(ofSize: 15)
         return lbl
     }()
 
@@ -44,7 +44,7 @@ class CastomTableViewCell: UITableViewCell {
         backgroundColor = UIColor.clear
         backeView.layer.cornerRadius = 5
         backeView.clipsToBounds = true
-        userImage.layer.cornerRadius = 44
+        userImage.layer.cornerRadius = 36
         userImage.clipsToBounds = true
     }
 
