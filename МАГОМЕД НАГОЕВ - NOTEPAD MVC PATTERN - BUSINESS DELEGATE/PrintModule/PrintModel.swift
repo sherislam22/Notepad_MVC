65f6efd3dd87fa58dd14aba7d424206e61b9b24a
Шерислам Талантбеков <talantbekovsherislam@gmail.com>
2022-08-21 20:18:39 +0600

    Update PrintModel.swift

--- МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/PrintModule/PrintModel.swift
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/PrintModule/PrintModel.swift
@@ -19,7 +19,30 @@ class PrintModel {
     }
     
     func createImage(text: String) {
+        let frame = CGRect(x: 0, y: 0, width: 595.2, height: 841.8)
+        var textPos = 0
+        
+        let myfont = CTFontCreateWithName("AvenirNext-bold" as CFString, 64, nil)
+        let attributes = [NSAttributedString.Key.font: myfont]
+        let attrString = NSAttributedString(string: text, attributes: attributes)
+        
 
+        let render = UIGraphicsImageRenderer(size: frame.size)
+        print(render.format.bounds)
+        while textPos < attrString.length {
+            let framesetter = CTFramesetterCreateWithAttributedString(attrString)
+            
+            let path = CGMutablePath()
+            path.addRect(CGRect(origin: .zero, size: frame.size))
+            
+            let ctframe = CTFramesetterCreateFrame(framesetter, CFRangeMake(textPos, 0), path, nil)
+            let frameRange = CTFrameGetVisibleStringRange(ctframe)
+            textPos += frameRange.length
+         
+        }
+        
+        printViewer.setImage(image: images)
     }
+    
     
 }
