5d8cff19ddcab77433e749a32bb54a9fb09f0915
Магомед Нагоев <magomed.nagoev@yandex.ru>
2022-08-27 22:25:45 +0600

    Update PrintViewer.swift

--- МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/PrintModule/PrintViewer.swift
+++ МАГОМЕД НАГОЕВ - NOTEPAD MVC PATTERN - BUSINESS DELEGATE/PrintModule/PrintViewer.swift
@@ -7,16 +7,26 @@
 
 import UIKit
 
-final class PrintViewer {
+final class PrintViewer: UIViewController {
     //MARK: - Properties
     private var printController: PrintController?
-    private var printInteractionController: UIPrintInteractionController
+    private let printInteractionController: UIPrintInteractionController
     
     //MARK: - Initialize
     init(text: String, font: UIFont) {
         printInteractionController = UIPrintInteractionController.shared
+        super.init(nibName: nil, bundle: nil)
         printController = PrintController(printViewer: self, text: text, font: font)
         printController?.callPrint(text: text, font: font)
+        printInteractionController.delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        printInteractionController.present(animated: true)
     }
     
     //MARK: - Methods
@@ -28,11 +38,26 @@ final class PrintViewer {
         
         let printInfo = UIPrintInfo(dictionary: nil)
         
-        printInfo.jobName = "printing an image"
+        printInfo.jobName = "Printing ..."
         printInfo.outputType = .general
         
         printInteractionController.printInfo = printInfo
         
         printInteractionController.printingItems = image
     }
+}
+
+extension PrintViewer: UIPrintInteractionControllerDelegate {
+    
+    func printInteractionControllerDidFinishJob(_: UIPrintInteractionController) {
+        let alert = UIAlertController.callStandartAlert(title: "Text was printed",
+                                                        message: "", completion: { [weak self] in
+            self?.dismiss(animated: true)
+        })
+
+        DispatchQueue.main.async { [weak self] in
+            self?.present(alert, animated: true)
+        }
+    }
+    
 }
